7e68cb7f5765b482e0a1fef5372531556d72e953
drewteller <81828027+drewteller@users.noreply.github.com>
2023-02-04 10:52:06 -0800

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,7 @@ export default function About() {
   return (
     <Layout>
       <h1>About Me</h1>
-      <p>I am a software developer and I love building great applications.</p>
+      <p>I am a software developer, and I love building great applications.</p>
       <p>I have been working in this field for several years and I am always looking for new challenges and opportunities to improve my skills and knowledge.</p>
     </Layout>
   )
