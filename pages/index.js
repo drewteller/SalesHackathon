226e442eeab30d09a7ce197c74fdc4a60f853534
drewteller <81828027+drewteller@users.noreply.github.com>
2023-02-04 10:46:18 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,30 @@
-import Container from '../components/container'
-import ExperienceList from '../components/experience-list'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllExperiencessForHome, getProfileForHome } from '../lib/api'
+import Navbar from './components/navbar'
+import Container from './components/container'
+import ExperienceList from './components/experience-list'
+import Intro from './components/intro'
+import Layout from './components/layout'
+import { getAllExperiencessForHome, getProfileForHome } from './lib/api'
 import Head from 'next/head'
 
-
 export default function Index({ allExperience, profile }) {
-  return (
-    <>
-      <Layout>
-        <Head>
-          <title>{profile.personal.name}'s Portfolio</title>
-        </Head>
-        <Container>
-          <Intro profile={profile} />
-          <ExperienceList experiences={allExperience} />
-        </Container>
-      </Layout>
-    </>
-  )
+return (
+<Layout>
+<Head>
+<title>{profile.personal.name}'s Portfolio</title>
+</Head>
+<Navbar />
+<Container>
+<Intro profile={profile} />
+<ExperienceList experiences={allExperience} />
+</Container>
+</Layout>
+)
 }
 
 export async function getStaticProps() {
-  const profile = await getProfileForHome()
-  const allExperience = await getAllExperiencessForHome()
-  return {
-    props: { allExperience, profile },
-  }
+const profile = await getProfileForHome()
+const allExperience = await getAllExperiencessForHome()
+return {
+props: { allExperience, profile },
+}
 }
