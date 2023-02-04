39990f4840a46e2410f4dade886cc299568f3002
drewteller <81828027+drewteller@users.noreply.github.com>
2023-02-04 09:27:50 -0800

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,19 +1,27 @@
 import Container from '../components/container'
 import Layout from '../components/layout'
+import { getAboutPageData } from '../lib/contentful'
 import Head from 'next/head'
 
-const About = () => (
+const About = ({ pageData }) => (
   <>
     <Layout>
       <Head>
         <title>About Page</title>
       </Head>
       <Container>
-        <h1>About Page</h1>
-        <p>Welcome to the About Page</p>
+        <h1>{pageData.title}</h1>
+        <p>{pageData.body}</p>
       </Container>
     </Layout>
   </>
 )
+
+export async function getStaticProps() {
+  const pageData = await getAboutPageData()
+  return {
+    props: { pageData },
+  }
+}
 
 export default About
