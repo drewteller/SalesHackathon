65ccfd60fcd8fe8256a55a9c8641de35a823e383
drewteller <81828027+drewteller@users.noreply.github.com>
2023-02-06 14:30:08 -0800

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -7,8 +7,7 @@ export default function About() {
   return (
     <Layout>
       <h1>About Me</h1>
-      <p>I am a software developer, and I love building great applications.</p>
-      <p>I have been working in this field for several years and I am always looking for new challenges and opportunities to improve my skills and knowledge.</p>
+      <p>Hi, I am Drew Teller A dad joke enthusiast, a growth operator, and the creator of TheProductLed newsletter. I love to teardown growth models and growth tactics of Product-Led Growth SaaS companies. --- I started off my career as a Mechanical Engineer where I spent 3 years designing HVAC systems by using CAD software, peforming energy models for commercial construction projects, and implemented effective software solutions to improve business outcomes. I culminated my tenure as a mechanical engineer by taking the state board PE exam and passed. I am currently still licensed state mechanical engineer. I moved to Sweden in 2018 to get my Masters in Management where I wrote my master's thesis in Strategic Thinking. During my time in Sweden, I founded and created the first donation based yoga company in Sweden where I taught yoga. The business operated at a 56% gross profit margin. [Test this](https://netlify.com). </p>
     </Layout>
   )
 }
