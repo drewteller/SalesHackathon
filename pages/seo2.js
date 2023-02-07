a59ce321a6ae89926d07e185c74934a4971c070f
drewteller <81828027+drewteller@users.noreply.github.com>
2023-02-06 17:14:43 -0800

    Update seo2.js

--- pages/seo2.js
+++ pages/seo2.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import styles from 'components/HowChatGPTCanHelpSEO.module.css';
+import styles from '/components/HowChatGPTCanHelpSEO.module.css';
 
 const SEO = () => (
   <div className={styles.container}>
