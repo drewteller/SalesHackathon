dc3553d5d0111ef66812399de72a01c4227d6627
drewteller <81828027+drewteller@users.noreply.github.com>
2023-02-07 09:01:51 -0800

    Update chatgpt.js

--- pages/chatgpt.js
+++ pages/chatgpt.js
@@ -4,6 +4,11 @@ import styles from '/components/HowChatGPTCanHelpSEO.module.css';
 const SEO = () => (
   <div className={styles.container}>
     <Head>
+  <Head>
+      <title>How ChatGPT can help debugging buil log errors</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+    </Head>
       <title>How ChatGPT can help debugging buil log errors | Netlify</title>
       <meta name="description" content="ChatGPT can assist in optimizing your website's content for search engines, improving visibility and attracting more organic traffic." />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
