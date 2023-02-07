c281157dd52b7988eedcdac2041baa0e24558241
drewteller <81828027+drewteller@users.noreply.github.com>
2023-02-06 16:48:47 -0800

    Update seopage.js

--- pages/seopage.js
+++ pages/seopage.js
@@ -2,48 +2,42 @@ import React from 'react';
 import Head from 'next/head';
 import styles from '/components/HowChatGPTCanHelpSEO.module.css';
 
-const HowChatGPTCanHelpSEO = () => (
 
-  <div>
+ const HowChatGPTCanHelpSEO = () => (
+  <div className="container">
     <Head>
       <title>How ChatGPT Can Help SEO</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
     </Head>
-    <div className="title">
-      <h1 className="text-center">How ChatGPT Can Help SEO</h1>
-    </div>
-    <div className="description">
-      <p className="text-center">
-        SEO is essential for websites to rank higher in search engines, and ChatGPT can provide assistance in optimizing your website's SEO.
-      </p>
-    </div>
-    <div className="seo-image">
-      <img src="https://statuslabs.com/wp-content/uploads/SEO-Pillar-Post-Art-.png" alt="SEO related image" />
-    </div>
-    <div className="what-is-chatgpt">
-      <h2>What is ChatGPT?</h2>
-      <p>
-        ChatGPT is a state-of-the-art language model developed by OpenAI. It's capable of generating human-like text based on the input provided to it.
-      </p>
-    </div>
-    <div className="how-can-chatgpt-help-seo">
-      <h2>How Can ChatGPT Help SEO?</h2>
-      <p>
-        ChatGPT can assist in optimizing a website's SEO in the following ways:
-      </p>
-      <ul>
-        <li>Generating unique and engaging content: ChatGPT can be used to generate high-quality, unique, and engaging content for a website. This can improve the overall user experience, which can have a positive impact on the website's ranking in search engines.</li>
-        <li>Keyword research: ChatGPT can be used to generate a list of keywords relevant to the website's content. These keywords can be used to optimize the website's content and improve its ranking in search engines.</li>
-        <li>Identifying areas for improvement: ChatGPT can analyze a website's existing content and identify areas for improvement. This can include suggestions for keyword optimization, improving website structure, and improving user experience.</li>
-      </ul>
-    </div>
-    <div className="conclusion">
-      <h2>Conclusion</h2>
-      <p>
-        In conclusion, ChatGPT can play a significant role in improving a website's SEO. By generating unique and engaging content, conducting keyword research, and identifying areas for improvement, ChatGPT can help websites rank higher in search engines and increase organic traffic.
-      </p>
-    </div>
+    <header className="header">
+      <h1 className="header__title text-center">How ChatGPT Can Help SEO</h1>
+    </header>
+    <main className="main">
+      <section className="section">
+        <h2 className="section__title">What is ChatGPT?</h2>
+        <p className="section__text">
+          ChatGPT is a state-of-the-art language model developed by OpenAI. It's capable of generating human-like text based on the input provided to it.
+        </p>
+      </section>
+      <section className="section">
+        <h2 className="section__title">How Can ChatGPT Help SEO?</h2>
+        <p className="section__text">
+          ChatGPT can assist in optimizing a website's SEO in the following ways:
+        </p>
+        <ul className="section__list">
+          <li className="section__list-item">Generating unique and engaging content: ChatGPT can be used to generate high-quality, unique, and engaging content for a website. This can improve the overall user experience, which can have a positive impact on the website's ranking in search engines.</li>
+          <li className="section__list-item">Keyword research: ChatGPT can be used to generate a list of keywords relevant to the website's content. These keywords can be used to optimize the website's content and improve its ranking in search engines.</li>
+          <li className="section__list-item">Identifying areas for improvement: ChatGPT can analyze a website's existing content and identify areas for improvement. This can include suggestions for keyword optimization, improving website structure, and improving user experience.</li>
+        </ul>
+      </section>
+      <section className="section">
+        <h2 className="section__title">Conclusion</h2>
+        <p className="section__text">
+          In conclusion, ChatGPT can play a significant role in improving a website's SEO. By generating unique and engaging content, conducting keyword research, and identifying areas for improvement, ChatGPT can help websites rank higher in search engines and increase organic traffic.
+        </p>
+      </section>
+    </main>
   </div>
 );
-export default HowChatGPTCanHelpSEO
+export default HowChatGPTCanHelpSEO;
