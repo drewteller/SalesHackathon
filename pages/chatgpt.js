188c34dc213d8a671e4030892b8e7b0342f67dbf
drewteller <81828027+drewteller@users.noreply.github.com>
2023-02-07 08:54:51 -0800

    Update chatgpt.js

--- pages/chatgpt.js
+++ pages/chatgpt.js
@@ -13,6 +13,9 @@ const SEO = () => (
       <img src="https://images.indianexpress.com/2022/12/ChatGPT_NEW_LEAD-1.jpg" alt="Netlify Logo" />
       <h1 className={styles.title}>How ChatGPT can Help with SEO</h1>
     </header>
+    <div className="seo-image">
+      <img src="https://images.indianexpress.com/2022/12/ChatGPT_NEW_LEAD-1.jpg" alt="SEO related image" />
+    </div>
     <main className={styles.main}>
       <p>
         ChatGPT can be used to assist in debugging build log errors in a number of ways. Here are a few examples:
